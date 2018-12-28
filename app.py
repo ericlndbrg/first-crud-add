494405563017d9020c542ce7f2c664868ff12458
Eric <ericrlindberg@gmail.com>
2018-12-28 15:20:28 -0500

    added an insertRecords function to handle the insertion of records into the database

--- app.py
+++ app.py
@@ -4,10 +4,17 @@ import sqlite3
 app = Flask(__name__)
 
 
+def insertRecords(name, number):
+    conn = sqlite3.connect("site.db")
+    c = conn.cursor()
+    c.execute("INSERT INTO contacts VALUES (:name, :phone_number)", {"name": name, "phone_number": number})
+    conn.commit()
+    conn.close()
+    return None
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    name = None
-    number = None
 
     conn = sqlite3.connect("site.db")
     c = conn.cursor()
@@ -17,18 +24,10 @@ def index():
     conn.close()
 
     if request.method == "POST":
-        name = request.form["name"]
-        number = request.form["number"]
-
-        conn = sqlite3.connect("site.db")
-        c = conn.cursor()
-        c.execute("INSERT INTO contacts VALUES (:name, :phone_number)", {"name": name, "phone_number": number})
-        conn.commit()
-        conn.close()
-
+        insertRecords(request.form["name"], request.form["number"])
         return redirect(url_for("index"))
 
-    return render_template("index.html", contacts=allContacts, headers=colHeaders, name=name, number=number)
+    return render_template("index.html", contacts=allContacts, headers=colHeaders)
 
 
 if __name__ == "__main__":
